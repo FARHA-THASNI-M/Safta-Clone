0ad31762e49321b6d8293958486a156f26944620
FARHA-THASNI-M <farhafaru8979@gmail.com>
2025-01-06 11:40:25 +0530

    edits

--- safta-portal-clone/src/pages/dashboard/dashboard.tsx
+++ safta-portal-clone/src/pages/dashboard/dashboard.tsx
@@ -4,25 +4,21 @@ import { useNavigate } from 'react-router-dom';
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
 
-  // Prevent going back to dashboard using the browser's back button
   useEffect(() => {
-    // Push a new state to the history stack when the component mounts
     window.history.pushState(null, '', window.location.href);
     
-    // Listen for popstate event (back or forward button)
     window.onpopstate = () => {
-      window.history.go(1); // Prevent going back
+      window.history.go(1);
     };
 
     return () => {
-      // Cleanup when the component is unmounted
       window.onpopstate = null;
     };
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('isLoggedIn'); // Clear login status
-    navigate('/'); // Redirect to login page
+    localStorage.removeItem('isLoggedIn'); 
+    navigate('/'); 
   };
 
   return (
