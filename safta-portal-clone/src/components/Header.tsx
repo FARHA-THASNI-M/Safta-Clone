1bc5ff1aca0fbd363de280d2a2145c09991be56e
FARHA-THASNI-M <farhafaru8979@gmail.com>
2025-01-07 14:52:09 +0530

    edits

--- safta-portal-clone/src/components/Header.tsx
+++ safta-portal-clone/src/components/Header.tsx
@@ -1,8 +1,9 @@
 import { Box } from "@mui/material";
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-const Header = () => {
+const Header: React.FC = () => {
 
   const navigate = useNavigate();
 
