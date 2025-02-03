8d6401afe3efc4522ba1f61079ee8d480d86f92d
FARHA-THASNI-M <farhafaru8979@gmail.com>
2025-02-03 11:56:33 +0530

    edits

--- safta-portal-clone/src/components/PrivateRoute.tsx
+++ safta-portal-clone/src/components/PrivateRoute.tsx
@@ -1,9 +1,4 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-
-interface PrivateRouteProps {
-  element: JSX.Element;
-}
 
 const PrivateRoute = () => {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
